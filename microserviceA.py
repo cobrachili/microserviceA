0f887b84c477b8e04d55d84166596c1daaa8de45
Stephan Gonzales <gonzstep@oregonstate.edu>
2025-05-24 23:40:52 +0000

    Fix the csv header names and refactored to deal with a list of played game being passed in and a finite list of genres to select from

--- microserviceA.py
+++ microserviceA.py
@@ -2,28 +2,33 @@ import time
 import random
 import csv
 
-def process_csv(input_file, played=None, genre=None):
+genre_list = ['action', 'strategy', 'adventure', 'casual', 'indie', 'rpg']
+
+def process_csv(input_file, genre=None):
     with open(input_file, 'r', newline='') as file:
         reader = csv.DictReader(file)
         games = []
         genreset = set()
         random_genre_list = []
         for row in reader:
-            if played is not None and row.get('played') == played:
-                games.append(row)
-            if played is None and genre is None:
+            if genre is None:
                 games.append(row)
             if genre is not None:
                 games.append(row)
-                genreset.add(row.get('genre'))
+                # genreset.add(row.get('genre'))
         if games == []:
             return []
         if genre is not None:
+            print("recieved random game csv:\n")
+            print(f"{games}\n")
             # print(genreset)
-            random_genre = random.choice(list(genreset))
+            # random_genre = random.choice(list(genreset))
+            random_genre = random.choice(genre_list)
+            print(f"random genre is: {random_genre}\n")
             for game in games:
-                if game.get('genre') == random_genre:
+                if game.get('Genre') == random_genre:
                     random_genre_list.append(game)
+            # print(random_genre_list)
             return random_genre_list
         # print(genreset)
         # print(games)  
@@ -31,7 +36,7 @@ def process_csv(input_file, played=None, genre=None):
 
 def write_game_csv(game):
     with open('game.csv', 'w', newline='') as gamefile:
-        fieldnames = ['game_title', 'played', 'genre']
+        fieldnames = ['Name', 'Genre', 'Rating', 'Release year']
         writer = csv.DictWriter(gamefile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow(game)
@@ -41,21 +46,29 @@ while True:
 
     randomgame = process_csv('random.csv')
     if randomgame:
+        print(f"recieved random game csv:\n")
+        print(f"{randomgame}\n")
+
         recommendation = random.choice(randomgame)
+        print(f"\nchosen: {recommendation}\n")
         write_game_csv(recommendation)
         with open('random.csv', 'w'):
             pass
 
-    randomgame_new = process_csv('random_new.csv', played='false')
+    randomgame_new = process_csv('random_new.csv')
     if randomgame_new:
+        print(f"recieved new random game csv:\n")
+        print(f"{randomgame_new}")
         recommendation_new = random.choice(randomgame_new)
+        print(f"\nchosen: {recommendation_new}\n")
         write_game_csv(recommendation_new)
         with open('random_new.csv', 'w'):
             pass
 
-    randomgame_genre = process_csv('random_genre.csv', played=None, genre=True)
+    randomgame_genre = process_csv('random_genre.csv', genre=True)
     if randomgame_genre:
         recommendation_genre = random.choice(randomgame_genre)
+        print(f"\nchosen: {recommendation_genre}\n")
         write_game_csv(recommendation_genre)
         with open('random_genre.csv', 'w'):
             pass  
